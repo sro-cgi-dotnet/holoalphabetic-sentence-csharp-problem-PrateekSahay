55c25594a2e63d744ab76a401758ea1198ad2337
Prateek Sahay <prateeksahay96@gmail.com>
2018-09-12 16:31:57 +0530

    Optimised

---
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace HoloalphabeticSentence
+{
+    public class HoloalphabeticSentence
+    {
+        // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
+        public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
+        {
+            input=input.ToLower();
+            foreach(char i in alphabets){
+               if (!(input.Contains(i))){
+                   return false;
+               }
+            }
+            return true;
+        }
+    }
+}
